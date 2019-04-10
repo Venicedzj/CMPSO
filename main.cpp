094005e656c23d4e288a7450d338b5bebde31259
Deng <546053297@qq.com>
2019-04-10 17:07:28 +0800

    2019-4-10

--- main.cpp
+++ main.cpp
@@ -3,35 +3,18 @@
 #include<stdlib.h>
 #include<time.h>
 #include<iomanip>
+#include<cmath>
+#include<limits>
 using namespace std;
-void initPtc();
-void PSO();
-double getFitnessVal(vector<double> POS);
-vector<double> get_gBest(vector<double> gBest);
-void calParticlesFitness(vector<double> pos, double& fitness);
-void update_V_POS();
-double LDW();
-double ranW();
-double ConcFDW();
-double ConvFDW();
-void print(vector<double> v);
 
-struct Particle
-{
-	vector<double> V;
-	vector<double> POS;
-	vector<double> pBest;
-	double fitness;
-	double pBest_ftns;
-};
-
-#define OBJECTIVE_NUM 18
+#define OBJECTIVE_NUM 2		//objectives' number
 #define PARTICLE_NUM 20		//particle's number
-#define V_MAX 4.0			//maximum velocity
-#define V_MIN -4.0			//minimum velocity
-#define DIMENSION 10		//spatial dimension
-#define POS_MAX 100.0		//maximum position coordinate
-#define POS_MIN -100.0		//minimum position coordinate
+#define NA 100
+//#define V_MAX 4.0			//maximum velocity
+//#define V_MIN -4.0		//minimum velocity
+//#define DIMENSION 10		//spatial dimension
+//#define POS_MAX 100.0		//maximum position coordinate
+//#define POS_MIN -100.0	//minimum position coordinate
 #define c1 4.0/3			//individual cognitive coefficient
 #define c2 4.0/3			//social learning coefficient
 #define c3 4.0/3
@@ -40,9 +23,116 @@ struct Particle
 #define Tmax 100			//maximum iterations number
 int T = 1;					//current iterations number
 #define t (double)T/Tmax	//evolution time
-vector<Particle> particles(PARTICLE_NUM);
-vector<double> gBest(DIMENSION);
+
+void initPtc();
+void CMPSO();
+double getFitnessVal(vector<double> POS);
+void update_gBest(vector<GBest> &gBest);
+void calParticlesFitness(vector<double> pos, double& fitness);
+void update_V_POS();
+double LDW();
+double ranW();
+double ConcFDW();
+double ConvFDW();
+void print(vector<double> v);
 inline double random(double a, double b) { return ((double)rand() / RAND_MAX) * (b - a) + a; }
+
+struct Particle
+{
+	vector<double> V;
+	vector<double> POS;
+	vector<double> pBest;
+	vector<double> pBest_ftns;
+	vector<double> fitness;
+	vector<double> Archive;
+	vector<double> Arc_ftns;
+};
+
+struct GBest
+{
+	vector<double> POS;
+	vector<double> fitness;
+};
+struct Archive {
+	vector<double> POS;
+	vector<double> fitness;
+};
+vector<vector<Particle>> particles(OBJECTIVE_NUM, vector<Particle>(PARTICLE_NUM));
+vector<GBest> gBest(OBJECTIVE_NUM);
+vector<vector<Archive>> ptc_archives(OBJECTIVE_NUM, vector<Archive>(PARTICLE_NUM));
+vector<Archive> archives(NA);
+
+class Function
+{
+public:
+	int dimension;
+	vector<double> pos_max;
+	vector<double> pos_min;
+	double v_max;
+	double v_min;
+
+	Function() {}
+
+	void InitParticle() {
+		for (int m = 0; m < OBJECTIVE_NUM; ++m) {
+			for (auto& ptc : particles[m]) {
+				for (int i = 0; i < dimension; i++) {
+					ptc.V.push_back(random(v_min, v_max));
+					ptc.POS.push_back(random(pos_min[i], pos_max[i]));
+				}
+				ptc.pBest = ptc.POS;
+				ptc.fitness = CalFitness(ptc.POS);
+				ptc.pBest_ftns = CalFitness(ptc.pBest);
+			}
+
+			int minmark = 0;
+			double minfitness = particles[m][0].pBest_ftns[m];
+			for (int i = 0; i < PARTICLE_NUM; i++) {
+				if (particles[m][i].pBest_ftns[m] < minfitness) {
+					minfitness = particles[m][i].pBest_ftns[m];
+					minmark = i;
+				}
+			}
+			gBest[m].POS = particles[m][minmark].pBest;
+			gBest[m].fitness = particles[m][minmark].pBest_ftns;
+		}
+	}
+	void UpdateArchive();
+	void Elitist_learning_strategy();
+	//virtual void InitParticle(vector<Particle> particles) = 0;
+	virtual vector<double> CalFitness(vector<double> pos) = 0;
+	~Function() {}
+};
+
+class ZDT1 : public Function
+{
+public:
+	int dimension = 30;
+	vector<double> pos_max;
+	vector<double> pos_min;
+	double v_max = 0.2 * (pos_max[0] - pos_min[0]);
+	double v_min = -v_min;
+
+	ZDT1() {
+		for (int i = 0; i < dimension; ++i) {
+			pos_max.push_back(1);
+			pos_min.push_back(0);
+		}
+	}
+
+	vector<double> CalFitness(vector<double> pos) {
+		vector<double> f(2);
+		f[0] = pos[0];
+		double gx = 0;
+		for (int i = 1; i < dimension; ++i) {
+			gx += pos[i];
+		}
+		gx = gx * 9 / (dimension - 1) + 1;
+		double hx = 1 - sqrt(f[0] / gx);
+		f[1] = gx * hx;
+		return f;
+	}
+};
 
 double(*wghtFunc[4])() = { LDW,ranW,ConcFDW,ConvFDW };
 /*linear decrement weights*/
@@ -56,16 +146,18 @@ double ConvFDW() { return wMin + (wMax - wMin) * (t - 1) * (t - 1); }
 
 int main() {
 	srand((int)time(0));
-	initPtc();
+	ZDT1 zdt1_func;
+	zdt1_func.InitParticle();
+	//initPtc(); <----update Archive
 	while (T <= Tmax) {
-		PSO();
+		CMPSO();
 		T++;
 	}
 	return 0;
 }
 
 /*initialize each particle's velocity, position, pBest and global best position gBest*/
-void initPtc() {
+/*void initPtc() {
 	for (auto& ptc : particles) {
 		for (int i = 0; i < DIMENSION; i++) {
 			ptc.V.push_back(random(V_MIN, V_MAX));
@@ -85,15 +177,71 @@ void initPtc() {
 		}
 	}
 	gBest = particles[minmark].pBest;
-}
+}*/
 
 /*steps of the PSO algorithm*/
-void PSO() {
-	gBest = get_gBest(gBest);
-	print(gBest);
+void CMPSO() {
+	for (int m = 0; m < OBJECTIVE_NUM; ++m) {
+		for (auto& ptc : particles[m]) {
+			//
+		}
+	}
 	update_V_POS();
+	update_gBest(gBest);
+	print(gBest);
 }
 
+void Function::UpdateArchive() {
+	vector<Archive> S;
+	Archive temp;
+	for (int m = 0; m < OBJECTIVE_NUM; ++m) {
+		for (auto ptc : particles[m]) {
+			temp.POS = ptc.pBest;
+			temp.fitness = ptc.pBest_ftns;
+			S.push_back(temp);
+		}
+	}
+	vector<Archive> old_archives = archives;
+	for (auto arc : old_archives) {
+		S.push_back(arc);
+	}
+	Elitist_learning_strategy();
+	vector<Archive> new_archives = archives;
+	for (auto arc : new_archives) {
+		S.push_back(arc);
+	}
+}
+void Function::Elitist_learning_strategy() {
+	int size = archives.size();
+	for (int i = 0; i < size; ++i) {
+		Archive E = archives[i];
+		int d = random(1, dimension);
+		E.POS[d] += (pos_max[d] - pos_min[d]) * Gaussian(0, 1);
+		if (E.POS[d] > pos_max[d]) E.POS[d] = pos_max[d];
+		if (E.POS[d] < pos_min[d]) E.POS[d] = pos_min[d];
+		E.fitness = CalFitness(E.POS);
+		archives[i] = E;
+	}
+}
+
+double Gaussian(double mu, double sigma) {
+	const double epsilon = numeric_limits<double>::min();
+	const double two_pi = 2.0 * 3.14159265358979323846;
+
+	static double z0, z1;
+	static bool generate;
+	generate = !generate;
+	if (!generate) return z1 * sigma + mu;
+
+	double u1, u2;
+	do {
+		u1 = rand() * (1.0 / RAND_MAX);
+		u2 = rand() * (1.0 / RAND_MAX);
+	} while (u1 <= epsilon);
+	z0 = sqrt(-2.0 * log(u1)) * cos(two_pi * u2);
+	z1 = sqrt(-2.0 * log(u1)) * sin(two_pi * u2);
+	return z0 * sigma + mu;
+}
 /*using position coordinates as parameter calculate the fitness value by fitness function*/
 double getFitnessVal(vector<double> POS) {
 	double fitness = 0;
@@ -110,7 +258,7 @@ void calParticlesFitness(vector<double> pos, double& fitness) {
 }
 
 /*calculate gBest after updating velocity and position*/
-vector<double> get_gBest(vector<double> gBest) {
+void update_gBest(vector<GBest> &gBest) {
 	double minimum = particles[0].fitness;
 	int flag = 0;
 	for (int i = 0; i < PARTICLE_NUM; i++) {
